4f666857f085f943da79bcee99592e9276d29d7a
alanphil2k01 <alanphilipose@gmail.com>
2021-11-09 23:27:14 +0530

    fixed setup script

--- awesome_setup.sh
+++ awesome_setup.sh
@@ -14,5 +14,9 @@ do
     stow $dot
 done
 
+#install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # All done
 echo "All Done!!"
